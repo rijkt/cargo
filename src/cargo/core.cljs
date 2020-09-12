aea6b988118cece6a9e1ccf5b72cad2a97a64b94
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2020-09-12 17:05:29 +0200

    adjust naming

--- src/cargo/core.cljs
+++ src/cargo/core.cljs
@@ -6,13 +6,14 @@
 
 (enable-console-print!)
 
-(defn create-grid [width height]
+(defn create-truck-area [width height]
   (let [container (.getElementById js/document "cargo-area")
         style     (.createAttribute js/document "style")]
-    (set! (.-value style) (str "width: " width "px; height: " height "px;"))
+    (set! (.-value style) (str "width: " width "px; height: " height "px;"
+                               "outline: solid black;"))
     (.setAttributeNode container style)))
 
-(defn button-handler []
-  (let [x (. (.getElementById js/document "width") -value)
-        y (. (.getElementById js/document "height") -value)]
-  (create-grid x y)))
+(defn create-truck []
+  (let [x (. (.getElementById js/document "truck-width") -value)
+        y (. (.getElementById js/document "truck-height") -value)]
+  (create-truck-area x y)))
