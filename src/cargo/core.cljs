747e96f73930a1d0e5b4321d4feaaef0bce5a6a8
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2020-09-12 19:15:27 +0200

    inline function

--- src/cargo/core.cljs
+++ src/cargo/core.cljs
@@ -6,14 +6,10 @@
 
 (enable-console-print!)
 
-(defn create-truck-area [width height]
-  (let [container (.getElementById js/document "cargo-area")
-        style     (.createAttribute js/document "style")]
-    (set! (.-value style) (str "width: " width "px; height: " height "px;"
-                               "outline: solid black;"))
-    (.setAttributeNode container style)))
-
 (defn create-truck []
-  (let [x (. (.getElementById js/document "truck-width") -value)
-        y (. (.getElementById js/document "truck-height") -value)]
-  (create-truck-area x y)))
+  (let [width (. (.getElementById js/document "truck-width") -value)
+        height (. (.getElementById js/document "truck-height") -value)
+        container (.getElementById js/document "cargo-area")
+        style     (.createAttribute js/document "style")]
+    (set! (.-value style) (str "width: " width "px; height: " height "px;" "outline: solid black;"))
+    (.setAttributeNode container style)))
