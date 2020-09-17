d5cf0350818d93ec842da8035e0e411cd45565b6
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2020-09-17 20:34:17 +0200

    add form to add cargo items

--- src/cargo/core.cljs
+++ src/cargo/core.cljs
@@ -9,7 +9,18 @@
 (defn create-truck []
   (let [width (. (.getElementById js/document "truck-width") -value)
         height (. (.getElementById js/document "truck-height") -value)
-        container (.getElementById js/document "cargo-area")
+        container (.getElementById js/document "truck-area")
         style     (.createAttribute js/document "style")]
     (set! (.-value style) (str "width: " width "px; height: " height "px;" "outline: solid black;"))
     (.setAttributeNode container style)))
+
+(defn create-cargo []
+  (let [width (. (.getElementById js/document "cargo-width") -value)
+        height (. (.getElementById js/document "cargo-height") -value)
+        container (.getElementById js/document "cargo-area")
+        style (.createAttribute js/document "style")
+        cargo (.createElement js/document "div")]
+    (set! (.-value style) (str "width: " width "px; height: " height "px;" "background-color: green;"))
+    (.setAttributeNode cargo style)
+    (.setAttribute cargo "draggable" "true")
+    (.appendChild container cargo)))
