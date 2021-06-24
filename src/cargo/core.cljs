a3689e9ef3bdf117ef1a2008f8d661bb17508e96
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2021-06-24 18:55:32 +0200

    add color selection and sensible defaults

--- src/cargo/core.cljs
+++ src/cargo/core.cljs
@@ -5,7 +5,10 @@
 
 (def state (atom {}))
 
-(defn create-truck []
+(defn create-truck
+  "Create loading area based on form inputs. Since creating a fabric Canvas changes the DOM,
+  this fn first deletes any existing canvas and creates a new one."
+  []
   (let [wrapper (.getElementById js/document "canvas-wrapper")
         new-canvas (.createElement js/document "canvas")]
     (set! (.-innerHTML wrapper) "") ; todo: this is not recommended, find a better way
@@ -19,20 +22,26 @@
 (defn create-cargo []
   (let [width (:cw @state)
         height (:ch @state)
+        color (:cc @state)
         canvas (:canvas @state)]
     (.add canvas
           (new (.-Rect js/fabric)
-               (clj->js {:width width :height height :fill "red" })))))
+               (clj->js {:width width :height height :fill color})))))
 
 (defn read-form! [id]
-  (int (. (.getElementById js/document id) -value)))
+  (. (.getElementById js/document id) -value))
   
 (defn update-form-state []
-  (let [truck-width (read-form! "truck-width")
-        truck-height (read-form! "truck-height")
-        cargo-width (read-form! "cargo-width")
-        cargo-height (read-form! "cargo-height")]
-    (swap! state assoc :tw truck-width :th truck-height :cw cargo-width :ch cargo-height)))
+  (let [truck-width (int (read-form! "truck-width"))
+        truck-height (int (read-form! "truck-height"))
+        cargo-width (int (read-form! "cargo-width"))
+        cargo-height (int (read-form! "cargo-height"))
+        cargo-color (str (read-form! "cargo-color"))]
+    (swap! state assoc :tw truck-width
+           :th truck-height
+           :cw cargo-width
+           :ch cargo-height
+           :cc cargo-color)))
 
 (defn setup-event-handlers []
 (let [inputs (array-seq (.getElementsByTagName js/document "input"))]
@@ -40,3 +49,4 @@
 
 (update-form-state) ; read data left in form after refresh
 (setup-event-handlers)
+(create-truck)
